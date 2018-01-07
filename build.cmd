59d11656e9e60208301085d551373784c9403238
Wouter Saaltink <wouter.saaltink@gmail.com>
2018-01-07 01:02:49 +0100

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -3,8 +3,8 @@ pushd %cd%
 set INSTALL_PREFIX=%1
 set COMPILER_TOOLSET=%2
 
-if "%COMPILER_TOOLSET%" == "" set COMPILER_TOOLSET = "MinGW Makefiles"
-if "%INSTALL_PREFIX%" == "" set INSTALL_PREFIX = "..\..\%COMPILER_TOOLSET%"
+if %COMPILER_TOOLSET% == "" set COMPILER_TOOLSET = "MinGW Makefiles"
+if %INSTALL_PREFIX% == "" set INSTALL_PREFIX = "..\..\%COMPILER_TOOLSET%"
 
 IF EXIST build\bullet3 GOTO doneClone
 
@@ -17,7 +17,7 @@ git clone https://github.com/bulletphysics/bullet3.git build\bullet3
 :doneClone
 
 cd build
-IF NOT EXIST "%COMPILER_TOOLSET%" mkdir "%COMPILER_TOOLSET%"
+IF NOT EXIST %COMPILER_TOOLSET% mkdir %COMPILER_TOOLSET%
 
 cd "%COMPILER_TOOLSET%"
 cmake -G "%COMPILER_TOOLSET%" -DCMAKE_INSTALL_PREFIX:PATH=%INSTALL_PREFIX% ..\
